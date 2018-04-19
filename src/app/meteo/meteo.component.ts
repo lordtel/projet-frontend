e81347608a4341ecd36cdd5e0b3fe48721d3a5da
noureddine.brahmi <noureddine.brahmi@gmail.com>
2018-04-19 15:05:04 -0400

    adding the town service

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import{ Town } from '../town';
+import { Town } from '../town'
+import { TownService } from '../town.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-meteo',
   templateUrl: './meteo.component.html',
-  styleUrls: ['./meteo.component.css']
+  styleUrls: ['./meteo.component.css'],
 })
 export class MeteoComponent implements OnInit {
-
-  town: Town={id: 1, name: 'Belfort', temperature: 12, icon: 'sunny'};
-  
-  constructor() { }
+  town: Town;
+  private townService: TownService;
+  constructor(private http: HttpClient) {
+	  this.townService = new TownService(http)
+  }
 
   ngOnInit() {
+    this.townService.getTown(1).subscribe(town => {
+      this.town=town;
+    });
   }
 
 }
--- src/app/town.service.ts
+++ src/app/town.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { Town } from './town';
 
 @Injectable()
 export class TownService {
+  
+  private URL = "http://lpa2sgadot.herokuapp.com/towns"
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
+  getTown(id : number) : Observable<Town> {
+    return this.http.get<Town>(`${this.URL}/${id}.json`);
+  }
 }
